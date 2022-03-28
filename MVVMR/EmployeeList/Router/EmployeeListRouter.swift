10236ec529f631c66a14cb9ca6f549f294253e3d
Lalit Kant <lalit.kant@honeywell.com>
2022-03-29 01:30:57 +0530

    Code Styling

--- MVVMR/EmployeeList/Router/EmployeeListRouter.swift
+++ MVVMR/EmployeeList/Router/EmployeeListRouter.swift
@@ -8,10 +8,9 @@
 import Foundation
 import UIKit
 
-/// Creating about Module to hold View controller
+/// Employee list module is to create instance of view controller and initiating view controller in view model router
 class EmployeeListModule {
-    /// create About Module
-    /// - Returns: FBAboutViewController
+    
     func createModule() -> UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let empVC = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
@@ -19,27 +18,21 @@ class EmployeeListModule {
         empVC.employeeViewModel.router?.viewController = empVC
         return empVC
     }
-   
 }
 
-/// More Router Protocol
-protocol EmployeeListRouterProtocol {
-    func routeToEmployeeDetailScreen(empData: EmployeeData?)
-}
-
-/// Router to hold View controller of More tab and it can be extended to include data to hold and pass to other screens
 class EmployeeListRouter {
     var viewController: UIViewController?
 }
 
+protocol EmployeeListRouterProtocol {
+    func routeToEmployeeDetailScreen(empData: EmployeeData?)
+}
+
 extension EmployeeListRouter: EmployeeListRouterProtocol {
     
-    /// Route to about Screen
     func routeToEmployeeDetailScreen(empData: EmployeeData?) {
         let empDetail = EmployeeDetailModule().createModule()
         empDetail.employeeDetailViewModel.empData = empData
         viewController?.navigationController?.pushViewController(empDetail, animated: true)
     }
-
-    
 }
--- MVVMR/SceneDelegate.swift
+++ MVVMR/SceneDelegate.swift
@@ -11,14 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        let windowScene = UIWindowScene(session: session, connectionOptions: connectionOptions)
-                self.window = UIWindow(windowScene: windowScene)
         
+        let windowScene = UIWindowScene(session: session, connectionOptions: connectionOptions)
+        self.window = UIWindow(windowScene: windowScene)
         // Override point for customization after application launch.
         let navigationController = UINavigationController(rootViewController:
                                                             EmployeeListModule().createModule())
